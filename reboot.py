1ee22ff6185b4a08be7fc1a0db755374fce303ba
Chris Steenkamp <cdsteenkamp@gmail.com>
2025-01-10 09:14:13 +0200

    Create rough script to reboot router.

---
+++ reboot.py
@@ -0,0 +1,70 @@
+import os
+from time import sleep
+
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# def sleep(n): ...
+
+def get_named_element(driver, class_name, element_name):
+    element = [
+        i
+        for i in list(driver.find_elements(By.CLASS_NAME, class_name))
+        if i.text == element_name
+    ][0]
+
+    return element
+
+def perform_reboot(url, password, headless: bool = True):
+    print("Starting browser...")
+    options = webdriver.FirefoxOptions()
+    if headless:
+        options.add_argument("--headless")
+    driver = webdriver.Firefox(options=options)
+    print("Browser started")
+    sleep(5)
+
+    print("Navigating to login page...")
+    driver.get(url)
+    sleep(3)
+
+    # Set password
+    password_field = driver.find_element(By.CLASS_NAME, "password-hidden")
+    sleep(1)
+    password_field.send_keys(password)
+    sleep(1)
+
+    # Login
+    print("Logging in...")
+    login = driver.find_element(By.CLASS_NAME, "button-button")
+    sleep(1)
+    login.click()
+    sleep(5)
+    print("Logged in")
+
+    # Navigate to reboot page
+    print("Loading device list...")
+    driver.get(f"{url}#reboot")
+    sleep(30)
+
+    # Click reboot
+    print("Rebooting...")
+    reboot_button = get_named_element(driver, "button-button", "REBOOT ALL")
+    reboot_button.click()
+    sleep(5)
+
+    # Confirm reboot
+    reboot_button = get_named_element(driver, "button-button", "Reboot")
+    reboot_button.click()
+    sleep(60)
+
+    driver.quit()
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    password = os.environ.get("PASSWORD")
+    url = os.environ.get("BASE_URL")
+
+    perform_reboot(url, password)
