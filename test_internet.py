837a9aec04058c51e39b1db3989681a056e9db5a
Chris Steenkamp <cdsteenkamp@gmail.com>
2025-01-11 10:24:01 +0200

    Format code with ruff.

--- test_internet.py
+++ test_internet.py
@@ -1,5 +1,6 @@
 import socket
 
+
 def check_host_and_port_are_reachable(host: str, port: int) -> bool:
     print(f"Checking connectivity: {host}... ", end="")
     try:
@@ -13,10 +14,13 @@ def check_host_and_port_are_reachable(host: str, port: int) -> bool:
         print(f"Failed with error: {e}")
         return False
 
+
 HOSTS = ["8.8.8.8", "8.8.4.4", "1.1.1.1", "1.0.0.1", "169.1.1.1", "169.1.1.2"]
+
 
 def run_checks():
     return any((check_host_and_port_are_reachable(host, 53) for host in HOSTS))
+
 
 if __name__ == "__main__":
     run_checks()
