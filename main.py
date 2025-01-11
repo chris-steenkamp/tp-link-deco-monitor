923e7948c6b5a481310c21c56162f93907951a13
Chris Steenkamp <cdsteenkamp@gmail.com>
2025-01-11 12:37:27 +0200

    Use a lock file to avoid the script being run concurrently.

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import fcntl
+
 from reboot import perform_reboot
 from test_internet import run_checks
 
-if not run_checks():
-    perform_reboot()
+LOCK_FILE = "/var/lock/check_connectivity.lock"
+
+with open(LOCK_FILE, "w") as lock:
+    try:
+        fcntl.flock(lock, fcntl.LOCK_NB | fcntl.LOCK_EX)
+    except IOError:
+        print("Reboot already in progress... Exiting.")
+    else:
+        if not run_checks():
+            perform_reboot()
