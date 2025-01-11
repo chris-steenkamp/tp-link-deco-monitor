dda2376a6f17fbcc4d1c544f2f14cf6a8ac62881
Chris Steenkamp <cdsteenkamp@gmail.com>
2025-01-11 17:27:34 +0200

    Update main program to switch to wifi network which is allowed to access router configuration.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import fcntl
 
 from reboot import perform_reboot
 from test_internet import run_checks
+from wifi_connection import switch_to_iot_network, switch_to_primary_network
 
 LOCK_FILE = "/var/lock/check_connectivity.lock"
 
@@ -12,4 +13,6 @@ with open(LOCK_FILE, "w") as lock:
         print("Reboot already in progress... Exiting.")
     else:
         if not run_checks():
+            switch_to_primary_network()
             perform_reboot()
+            switch_to_iot_network()
